07099d908f44c8871f38c496023c3b27c5375c9e
Kónya Milán <b0konmil@vasvari.org>
2024-11-20 15:11:36 +0100

    3.feladat

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ import expressRouter from './routes/express.js'
 import greetingRouter from './routes/greeting.js'
 import nodejsRouter from './routes/nodejs.js'
 import bodyParser from 'body-parser'
+import path from 'path'
+import {fileURLToPath} from 'url'
 
 const app=express()
 const PORT = 3000
 const router = express.Router()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -17,6 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use("/",(req, res) => {
     res.send("Hi there!")
+    res.sendFile(path.join(__dirname,'..', 'public', 'index.html'))
 });
 
 
